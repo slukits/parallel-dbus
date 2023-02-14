5e6e005928e1fcadeb66e4093d88419fb87c9310
Stephan Lukits <stephan.lukits@gmail.com>
2023-02-14 21:05:00 +0200

    rm: github workflows
    go 1.20 did build but the environment doesn't have NetworkManager
    available and also no wifi, which is kinda logical :( now that I come to
    think of it.

--- main_test.go
+++ main_test.go
@@ -20,7 +20,6 @@ func (s *RequestHandler) Prints_help_if_no_sub_command_given(t *T) {
 func (s *RequestHandler) Fails_on_unknown_sub_command(t *T) {
 	expPnc, expErr, subCmd := "fatal mock panic", "", "unknown"
 	defer func() {
-		t.Log(expErr)
 		t.Eq(expPnc, recover().(string))
 		t.Contains(expErr, fmt.Sprintf(subErr, subCmd))
 	}()
